40bf7671a9687130bee728638b85b38f162e3b04
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-17 22:32:30 +0000

    add SqlRunner class for PostgreSQL communication

---
+++ db/sql_runner.rb
@@ -0,0 +1,29 @@
+require('pg')
+require('logger')
+
+
+class SqlRunner
+
+  def self.run(sql)
+    begin              # coping with errors from here on
+      db = PG.connect( 
+        { 
+          dbname: 'cccinema', 
+          host: 'localhost'
+        }
+      ) 
+      result = db.exec(sql)
+    rescue           # do something with any errors
+      # do nothing today
+    ensure           # even if an error, do the following
+      db.close
+    end
+    # Log sql commands to a log file
+    logger = Logger.new('sql.log')
+    logger.info('SQL ' + sql + ' SQL')
+    logger.close
+    return result
+
+  end
+
+end
