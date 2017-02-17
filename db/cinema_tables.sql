65893ac2cf17e58b328e2def94140c4a341d1b19
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-17 22:23:48 +0000

    add some initial data to database using sql file

--- db/cinema_tables.sql
+++ db/cinema_tables.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS customers;
 
 CREATE TABLE customers ( 
   id SERIAL PRIMARY KEY,
-  funds DECIMAL(2),
+  funds DECIMAL(8,2),
   name VARCHAR(255)
 );
 
@@ -26,6 +26,14 @@ CREATE TABLE screenings (
   film_id INT REFERENCES films(id) ON DELETE CASCADE,
   showtime TIMESTAMP,
   screen INT2,
-  price DECIMAL(2)
+  price DECIMAL(8,2)
 );
+
+INSERT INTO customers ( name, funds ) VALUES ('testing_A Smith', 123.45);
+INSERT INTO customers ( name, funds ) VALUES ('testing_B Jones', 1023.45);
+INSERT INTO customers ( name, funds ) VALUES ('testing_C Evans', 12300.45);
+
+INSERT INTO films ( title, duration_mins) VALUES ('testing_Star Wars', 150);
+INSERT INTO films ( title, duration_mins) VALUES ('testing_Dr Strangelove, or: How I learned to stop worrying and love the bomb', 130);
+INSERT INTO films ( title, duration_mins) VALUES ('testing_The Lego Batman Movie', 92 );
 
